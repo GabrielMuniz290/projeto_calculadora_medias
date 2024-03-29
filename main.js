db45d93802e5225e83f28071f1cfd557b1d2689d
Gabriel Muniz <gabrielmunizdeoliveira29@gmail.com>
2024-03-29 11:02:23 -0300

    fix

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function atualizatabela(){
 
 function atualizamediafinal(){
     const mediafinal = calculamediafinal();
-    document.getElementById('media-final-valor').innerHTML = mediafinal;
+    document.getElementById('media-final-valor').innerHTML = mediafinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediafinal >= 7 ? spanaprovado : spanreprovado;
 }
 
